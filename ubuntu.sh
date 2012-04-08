c5687324b8456922556c6d60c7a2c080272bf93d
gnagel <glenn@mercury-wireless.com>
2012-04-07 21:28:22 -0400

    hacking setup

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,4 @@
-# Install the latest version of tor, with the tor2web flag
+# Install the latest version of tor
 sudo apt-add-repository http://deb.torproject.org/torproject.org
 gpg --keyserver keys.gnupg.net --recv 886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
@@ -11,6 +11,18 @@ sudo apt-get -y build-dep tor >>/tmp/install.log
 git clone git://git.torproject.org/debian/tor.git
 cd tor
 ./configure && make && sudo make install
+sudo sh -c "ln -s /etc/tor/torrc /usr/local/etc/tor/torrc"
+sudo sh -c "cat torrc.config >> /etc/tor/torrc"
+sudo sh -c "echo '_tor hard nofile 32768' >> /etc/security/limits.conf"
+
+# Setup the firewall
+sudo mkdir -p /etc/sysconfig
+sudo sh -c "iptables-save > /etc/sysconfig/iptables.default"
+sudo sh -c "iptables-save > /etc/sysconfig/iptables.test"
+sudo sh -c "cat iptables.config >> /etc/sysconfig/iptables.test"
+sudo sh -c "iptables-restore < /etc/sysconfig/iptables.test"
+sudo sh -c "iptables -L" # verify the rules are correct
+sudo sh -c "iptables-save > /etc/sysconfig/iptables"
 
 sudo /etc/init.d/tor restart
 
